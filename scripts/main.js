7e1267d408a55d4cce9d85202dc63681e15a79ba
swervo <swervo@papersnail.co.uk>
2012-09-24 15:42:00 +0100

    first initial framework

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,56 @@
+var thisApp = (function() {
+    var thisApp = {};
+    var DEBUG = true;
+    var $mainContainer;
+    
+    
+    function init(args) {
+        $mainContainer = $("#mainContainer")
+        $.getJSON("data/beers.json").success(function(aData) {
+            $.each(aData, function(idx, aBeer){
+                var tile = $("<div class='beerTile'>" + aBeer.beerName + "</div>");
+                $mainContainer.append(tile);
+            });
+            
+            initialiseLayout();
+        })
+        .error(function() {
+             print("json load error"); 
+        })
+        .complete(function() {
+            print("data load done");
+        });
+    }
+    
+    function initialiseLayout() {
+        $mainContainer.isotope({
+            itemSelector: ".beerTile",
+            layoutMode: "fitRows"
+        });
+    }
+    
+    function print(aString) {
+        if (DEBUG) {
+            console.log(aString);
+        }
+        
+    }
+    
+    
+    
+    return {
+        init: function() {
+            init();
+        },
+        
+        reset: function(args) {
+            
+        }
+    }
+})()
+
+
+
 $("document").ready(function(){
-    console.log("get the party started");
-})
+    thisApp.init();
+});
