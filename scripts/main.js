46dd16c2e0ff15c61eeb3e0f3fdd17503e5a6110
swervo <swervo@papersnail.co.uk>
2012-10-08 11:29:53 +0100

    adding in sorting functionality

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,19 @@ var thisApp = (function() {
         
         $sortLinks = $('#sortBy li').on("click", changeLayout);
         
-        console.log($sortLinks);
-        
         $.getJSON("data/beers.json").success(function(aData) {
             $.each(aData, function(idx, aBeer){
                 var tile = $("<div class='beerTile'></div>").css("background-image", "url(assets/" + aBeer.image + ")");
                 var beerName = $("<h3 class='title'>"+ aBeer.beerName +"</h3>");
-                var beerDetails = $("<p>" + aBeer.notes + "</p>")
+                var beerDetails = $("<p>" + aBeer.notes + "</p>");
+                var beerStrength = $("<span class='strength'>" + aBeer.strength + "%" + "</span>");
                 
                 // tile.append(bottleShot);
                 tile.append(beerName);
                 tile.append(beerDetails);
+                tile.append(beerStrength);
                 $mainContainer.append(tile);
+                
             });
             
             initialiseLayout();
@@ -35,8 +36,14 @@ var thisApp = (function() {
     
     function changeLayout(e) {
         console.log($(this).data());
+        var nodeData = $(this).data();
+        var sortObj = {};
+        sortObj.sortBy = nodeData.optionValue;
+        sortObj.sortAscending = nodeData.ascending;
+        // $mainContainer.isotope(sortObj);
         $mainContainer.isotope({
-            sortBy: "name"
+            sortBy: nodeData.optionValue,
+            sortAscending: nodeData.ascending
         });
     }
     
@@ -44,10 +51,14 @@ var thisApp = (function() {
         $mainContainer.isotope({
             itemSelector: ".beerTile",
             layoutMode: "fitRows",
-            getSortData : {
-                name : function ( $elem ) {
+            getSortData: {
+                name: function ($elem) {
                     return $elem.find('.title').text();
-                }
+                },
+                strength: function($elem) {
+                    return parseFloat($elem.find('.strength').text());
+                },
+                
             }
         });
     }
